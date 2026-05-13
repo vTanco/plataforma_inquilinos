b4b99df4eba871c7d113283fd48821342bb0d0fb
Vicente Tanco <vicente.tanco@edu.uah.es>
2026-05-13 18:18:16 +0100

    security: move PayPal keys to env vars, add .env to gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="105035380421-9jcph3rp9qug6d4slbrlledptjm2er90.apps.googleusercontent.com">
-      <PayPalScriptProvider options={{ clientId: "AeDDobUtJao6wEW0hzoKMvCOujVofou-b7yF58auEUISR7Rc2fNrQMQTZIUtyjmzaurSHZvJgG_g8bz6", currency: "EUR", intent: "capture", components: "buttons" }}>
+      <PayPalScriptProvider options={{ clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID, currency: "EUR", intent: "capture", components: "buttons" }}>
         <App />
       </PayPalScriptProvider>
     </GoogleOAuthProvider>
